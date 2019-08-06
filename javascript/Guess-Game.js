b56edfb82d575c18c25c4a0ce66ca0be877a0244
Devan Irvin <devanirvin@yahoo.com>
2019-08-06 18:36:53 -0400

    updated files

--- javascript/Guess-Game.js
+++ javascript/Guess-Game.js
@@ -1,56 +1,80 @@
-var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
 
-var wins = 0
+var wins = 0;
 
-var losses = 0
+var losses = 0;
 
-var computerPick
+var computerPick;
 
-var humanPick
+var humanPick;
 
-var guess = []
+var guess = [];
 
-var guessLeft = 9
+var guessLeft = 9;
 
-guessGame()
+guessGame();
 
-function guessGame () {
-  computerPick = letters[Math.floor(Math.random() * 26)]
-  console.log(computerPick)
-  getguess()
-  function getguess () {
-    document.onkeyup = function (event) {
-      var humanPick = event.key
+function guessGame() {
+  computerPick = letters[Math.floor(Math.random() * 26)];
+  console.log(computerPick);
+  getguess();
+  function getguess() {
+    document.onkeyup = function(event) {
+      var humanPick = event.key;
       guess.push(humanPick.toUpperCase());
-      console.log(humanPick)
+      console.log(humanPick);
 
       if (humanPick.toUpperCase() === computerPick) {
-        guessLeft = 9
-        wins++
-        document.getElementById('wins').innerHTML = ('Wins: ') + wins
-        reset()
-      } else calcLosses()
-    }
+        guessLeft = 9;
+        wins++;
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+        reset();
+      } else calcLosses();
+    };
   }
 
-  function calcLosses () {
-    document.getElementById('guess').innerHTML = ('Your Guesses:') + guess
-    guessLeft--
-    document.getElementById('guessLeft').innerHTML = ('Guess left: ') + guessLeft
+  function calcLosses() {
+    document.getElementById("guess").innerHTML = "Your Guesses:" + guess;
+    guessLeft--;
+    document.getElementById("guessLeft").innerHTML = "Guess left: " + guessLeft;
     if (guessLeft === 0) {
-      alert('Computer Letter is ' + computerPick)
-      losses++
-      document.getElementById('losses').innerHTML = ('Losses: ') + losses
-      reset()
-    }
-    else getguess()
+      alert("Computer Letter is " + computerPick);
+      losses++;
+      document.getElementById("losses").innerHTML = "Losses: " + losses;
+      reset();
+    } else getguess();
   }
 
-  function reset () {
+  function reset() {
     guess = [];
-    guessLeft = 9
-    guessGame()
+    guessLeft = 9;
+    guessGame();
   }
 }
-
-guessGame()
